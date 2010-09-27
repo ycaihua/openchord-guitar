2b6836ade0ee3c0c9817bf215fe6085329a5af47
alan.chatham <alan.chatham@gmail.com>
2010-09-27 07:05:04 +0000

    Adding MIDI code.  This is going to get ugly; realistically, the whole repository needs to be re-organized...

---
+++ V1/SourceCode/MIDI/default/dep/main.o.d
@@ -0,0 +1,80 @@
+main.o: ../main.c ../V1.h \
+  c:/winavr-20090313/lib/gcc/../../avr/include/avr/eeprom.h \
+  c:/winavr-20090313/lib/gcc/../../avr/include/avr/io.h \
+  c:/winavr-20090313/lib/gcc/../../avr/include/avr/sfr_defs.h \
+  c:/winavr-20090313/lib/gcc/../../avr/include/inttypes.h \
+  c:/winavr-20090313/lib/gcc/../../avr/include/stdint.h \
+  c:/winavr-20090313/lib/gcc/../../avr/include/avr/iom168.h \
+  c:/winavr-20090313/lib/gcc/../../avr/include/avr/iomx8.h \
+  c:/winavr-20090313/lib/gcc/../../avr/include/avr/portpins.h \
+  c:/winavr-20090313/lib/gcc/../../avr/include/avr/common.h \
+  c:/winavr-20090313/lib/gcc/../../avr/include/avr/version.h \
+  c:/winavr-20090313/lib/gcc/../../avr/include/avr/fuse.h \
+  c:/winavr-20090313/lib/gcc/../../avr/include/avr/lock.h \
+  c:\winavr-20090313\bin\../lib/gcc/avr/4.3.2/include/stddef.h \
+  c:/winavr-20090313/lib/gcc/../../avr/include/string.h \
+  ../guitarInitFunctions.h ../pindef.h ../configAndConstants.h \
+  ../MIDIInterface.h \
+  c:/winavr-20090313/lib/gcc/../../avr/include/avr/pgmspace.h \
+  ../usbdrv/usbconfig.h ../usbdrv/usbdrv.h ../usbdrv/usbconfig.h \
+  ../usbdrv/usbportability.h \
+  c:/winavr-20090313/lib/gcc/../../avr/include/avr/wdt.h \
+  c:/winavr-20090313/lib/gcc/../../avr/include/avr/interrupt.h \
+  c:/winavr-20090313/lib/gcc/../../avr/include/util/delay.h \
+  c:/winavr-20090313/lib/gcc/../../avr/include/util/delay_basic.h
+
+../V1.h:
+
+c:/winavr-20090313/lib/gcc/../../avr/include/avr/eeprom.h:
+
+c:/winavr-20090313/lib/gcc/../../avr/include/avr/io.h:
+
+c:/winavr-20090313/lib/gcc/../../avr/include/avr/sfr_defs.h:
+
+c:/winavr-20090313/lib/gcc/../../avr/include/inttypes.h:
+
+c:/winavr-20090313/lib/gcc/../../avr/include/stdint.h:
+
+c:/winavr-20090313/lib/gcc/../../avr/include/avr/iom168.h:
+
+c:/winavr-20090313/lib/gcc/../../avr/include/avr/iomx8.h:
+
+c:/winavr-20090313/lib/gcc/../../avr/include/avr/portpins.h:
+
+c:/winavr-20090313/lib/gcc/../../avr/include/avr/common.h:
+
+c:/winavr-20090313/lib/gcc/../../avr/include/avr/version.h:
+
+c:/winavr-20090313/lib/gcc/../../avr/include/avr/fuse.h:
+
+c:/winavr-20090313/lib/gcc/../../avr/include/avr/lock.h:
+
+c:\winavr-20090313\bin\../lib/gcc/avr/4.3.2/include/stddef.h:
+
+c:/winavr-20090313/lib/gcc/../../avr/include/string.h:
+
+../guitarInitFunctions.h:
+
+../pindef.h:
+
+../configAndConstants.h:
+
+../MIDIInterface.h:
+
+c:/winavr-20090313/lib/gcc/../../avr/include/avr/pgmspace.h:
+
+../usbdrv/usbconfig.h:
+
+../usbdrv/usbdrv.h:
+
+../usbdrv/usbconfig.h:
+
+../usbdrv/usbportability.h:
+
+c:/winavr-20090313/lib/gcc/../../avr/include/avr/wdt.h:
+
+c:/winavr-20090313/lib/gcc/../../avr/include/avr/interrupt.h:
+
+c:/winavr-20090313/lib/gcc/../../avr/include/util/delay.h:
+
+c:/winavr-20090313/lib/gcc/../../avr/include/util/delay_basic.h:
